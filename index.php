3742ca92f55477a4a7a05904fc70af6f1afa3ddb
daniels42 <106458868+daniels42@users.noreply.github.com>
2022-08-24 18:22:13 -0300

    Update index.php

--- index.php
+++ index.php
@@ -8,7 +8,8 @@
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@300;400;700&display=swap" rel="stylesheet">
-<link rel="stylesheet" href="style-index1.css">
+<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;700&family=Raleway:wght@300;400;700&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="style-index.css">
 <body>
 
 <?php
