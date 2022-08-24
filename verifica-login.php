55a0af585d7e90c43d65a7441561136815721efe
daniels42 <106458868+daniels42@users.noreply.github.com>
2022-08-24 18:13:34 -0300

    Update verifica-login.php

--- verifica-login.php
+++ verifica-login.php
@@ -1,13 +1,22 @@
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Raleway:wght@300;400;700&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;700&family=Raleway:wght@300;400;700&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="css/style-verifica-login1.css">
 <?php
-
 if(!isset($_SESSION)) {
     session_start();
 }
-
 if(!isset($_SESSION['autorizar'])) {
-    die("Você não pode acessar esta página porque não está logado.<p><a href=\"login.php\">Entrar</a></p> </br> <p><a href=\"registro.php\">Cadastrar</a></p");
-
+    die(
+        "<body>
+        <div>
+        <p> Você não pode acessar esta página porque não está logado </p> 
+        <br>
+        <center> <button> <a href=\"login.php\"> Entrar </a> </button> </center> 
+        <br><img height=50 src=img/logopeixe.png><br> 
+        <center> <button> <a href=\"registro.php\"> Cadastrar </a> </button </center> 
+        </div>
+        </body>");
 }
-
-
 ?>
