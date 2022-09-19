e5d3be720e16a408ed9c8e5e46bcfbede5fc7f46
Bonelli1 <felipe2bbrr@gmail.com>
2022-09-19 19:32:40 -0300

    .

--- menu.php
+++ menu.php
@@ -4,7 +4,7 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="#">
+ 
 
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 
@@ -39,6 +39,9 @@
       
       <li class="nav-item">
         <a class="nav-link" href="form_contato.php"> Contato </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="quemsomos.php"> Quem Somos </a>
       </li>
     </ul>
     
---
+++ quemsomos.php
@@ -0,0 +1,108 @@
+<?php 
+   
+    include("conexao.php");
+    include("menu.php");
+   
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+        <meta charset="utf-8">
+		<title>SP SUBMERSO - Quem Somos</title>
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="css/style-quemsomos.css">
+		<!-- CSS only -->
+
+ 
+</head>
+<body>
+      <!--Sobre-->
+	  <div class="container">
+	  <div class="row featurette">
+            <div class="col-md-7">
+                <h2 class="featurette-heading">Quem Somos</h2>
+                <p class="lead">Lorem ipsum aenean donec elit donec risus sagittis dictumst iaculis nec consectetur, quisque ultrices imperdiet morbi iaculis torquent nulla posuere ante. quisque habitasse diam fringilla cras vitae auctor, est scelerisque purus curae quisque laoreet, fringilla tristique pellentesque 
+                    <br /><br />
+                    Lorem ipsum aenean donec elit donec risus sagittis dictumst iaculis nec consectetur, quisque ultrices imperdiet morbi iaculis torquent nulla posuere ante. quisque habitasse diam fringilla cras vitae auctor, est scelerisque purus curae quisque laoreet, fringilla tristique pellentesque 
+                    <br /><br />
+                    Lorem ipsum aenean donec elit donec risus sagittis dictumst iaculis nec consectetur, quisque ultrices imperdiet morbi iaculis torquent nulla posuere ante. quisque habitasse diam fringilla cras vitae auctor, est scelerisque purus curae quisque laoreet, fringilla tristique pellentesque </p>
+            </div>
+            <div class="col-md-5">
+                <img class="featurette-image img-fluid rounded-top mx-auto" src="img/escritorio1.jpg" 
+                    >
+            </div>
+        </div>
+</div>
+
+<div id="team-area">
+        <div class="container">
+          <div class="row">
+            <div class="col-12">
+              <h3 class="main-title">Nosso time</h3>
+            </div>
+            <div class="col-md-3">
+              <div class="card">
+                <img src="img/escritorio1.jpg" class="card-img-top" alt="Imagem de Perfil 1">
+                <div class="card-body">
+                  <h5 class="card-title">André Neves</h5>
+                  
+                </div>
+              </div>
+            </div>
+            <div class="col-md-3">
+              <div class="card">
+                <img src="img/escritorio1.jpg" class="card-img-top" alt="Imagem de Perfil 2">
+                <div class="card-body">
+                  <h5 class="card-title">Daniel Henrique</h5>
+                  
+                </div>
+              </div>
+            </div>
+            <div class="col-md-3">
+              <div class="card">
+                <img src="img/escritorio1.jpg" class="card-img-top" alt="Imagem de Perfil 3">
+                <div class="card-body">
+                  <h5 class="card-title">Diego Eusébio</h5>
+                  
+                </div>
+              </div>
+            </div>
+            <div class="col-md-3">
+              <div class="card">
+                <img src="img/escritorio1.jpg" class="card-img-top" alt="Imagem de Perfil 4">
+                <div class="card-body">
+                  <h5 class="card-title">Fabricio Souza</h5>
+                  
+                </div>
+              </div>
+            </div>
+			<div class="col-md-3">
+              <div class="card">
+                <img src="img/escritorio1.jpg" class="card-img-top" alt="Imagem de Perfil 4">
+                <div class="card-body">
+                  <h5 class="card-title">Felipe Bonelli</h5>
+                  
+                </div>
+              </div>
+            </div>
+			<div class="col-md-3">
+              <div class="card">
+                <img src="img/escritorio1.jpg" class="card-img-top" alt="Imagem de Perfil 4">
+                <div class="card-body">
+                  <h5 class="card-title">Leonardo Alves</h5>
+                  
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+</body>
+
+<?php
+include("rodape.php")
+?>
+</html>
+
